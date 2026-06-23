cc10c845488f6ecbc331cae154a6c513c542218e
HemendraRoy <loaderbox2000@gmail.com>
2026-06-23 21:47:46 +0530

    Support for online API

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { env } from 'process';
 
 export default defineConfig({
   plugins: [react()],
@@ -13,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/execute': {
-        target: 'http://localhost:3001',
+        target: env.VITE_API_URL || 'http://localhost:3001',
         changeOrigin: true,
       },
     },
